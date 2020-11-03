ee644fa4250bb3f66cb242065e7b3e5472c7176f
Luciano <luciano_melopereira@hotmail.com>
2020-11-03 20:46:57 -0300

    Created DataContext

---
+++ PhoneBook/Data/PhoneBookContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Data
+{
+    public class PhoneBookContext : DbContext
+    {
+        public PhoneBookContext(DbContextOptions<PhoneBookContext> options ) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contact { get; set; }
+    }
+}
